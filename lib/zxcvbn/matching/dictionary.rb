57af21106cdba7f0abc641586962ed37c38f0e09
Steve Hodgkiss <steve@hodgkiss.me>
2012-09-19 11:20:52 +1000

    Use ranked dictionaries in dictionary matcher

--- lib/zxcvbn/matching/dictionary.rb
+++ lib/zxcvbn/matching/dictionary.rb
@@ -4,8 +4,8 @@ module Zxcvbn
     # the lowercased password in the dictionary
 
     class Dictionary
-      def initialize(dictionary)
-        @dictionary = dictionary
+      def initialize(ranked_dictionary)
+        @ranked_dictionary = ranked_dictionary
       end
 
       def matches(password)
@@ -15,8 +15,13 @@ module Zxcvbn
         (0..password_length).each do |i|
           (i...password_length).each do |j|
             word = lowercased_password[i..j]
-            if @dictionary.include?(word)
-              results << Match.new(:matched_word => word)
+            if @ranked_dictionary.has_key?(word)
+              results << Match.new(:matched_word => word,
+                                   :token => word,
+                                   :i => i,
+                                   :j => j,
+                                   :rank => @ranked_dictionary[word],
+                                   :pattern => 'dictionary')
             end
           end
         end
--- spec/matching/dictionary_spec.rb
+++ spec/matching/dictionary_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Zxcvbn::Matching::Dictionary do
   let(:matcher) { described_class.new(dictionary) }
-  let(:dictionary) { Zxcvbn::FREQUENCY_LISTS['english'] }
+  let(:dictionary) { Zxcvbn::RANKED_DICTIONARIES['english'] }
 
   it 'finds all the matches' do
     matches = matcher.matches('whatisinit')
@@ -10,5 +10,24 @@ describe Zxcvbn::Matching::Dictionary do
     expected_matches = ['wha', 'what', 'ha', 'hat', 'a', 'at', 'tis', 'i', 'is',
                        'sin', 'i', 'in', 'i', 'it']
     matches.map(&:matched_word).should == expected_matches
+  end
+
+  context 'integration' do
+    def dictionary_match(password, dictionary)
+      method_invoker.eval_convert_object(%'dictionary_match("#{password}", build_ranked_dict(#{dictionary}))')
+    end
+
+    %w[ viking briansmith4mayor ].each do |password|
+      it "gives back the same results for #{password}" do
+        js_results = dictionary_match(password, 'english')
+        ruby_results = matcher.matches(password)
+        js_results.count.should eq ruby_results.count
+        %w[ matched_word token i j rank pattern ].each do |attribute|
+          js_results.each_with_index do |js_result, index|
+            js_result[attribute].should eq ruby_results[index].send(attribute)
+          end
+        end
+      end
+    end
   end
 end
