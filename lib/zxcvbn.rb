025ce380c0affa99a28ea5151ad48203670030a4
Steve Hodgkiss <steve@hodgkiss.me>
2012-09-19 11:15:12 +1000

    Build ranked dictionaries

--- lib/zxcvbn.rb
+++ lib/zxcvbn.rb
@@ -10,6 +10,24 @@ module Zxcvbn
   ADJACENCY_GRAPHS = YAML.load(DATA_PATH.join('adjacency_graphs.yaml').read)
   FREQUENCY_LISTS = YAML.load(DATA_PATH.join('frequency_lists.yaml').read)
 
+  RANKED_DICTIONARIES = {}
+
+  def self.build_ranked_dictionaries
+    FREQUENCY_LISTS.each do |dict_name, words|
+      RANKED_DICTIONARIES[dict_name] = build_ranked_dictionary(words)
+    end
+  end
+
+  def self.build_ranked_dictionary(words)
+    {}.tap do |result|
+      i = 1
+      words.each do |word|
+        result[word] = i
+        i += 1
+      end
+    end
+  end
+
   def zxcvbn(password)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require 'zxcvbn'
 
 Dir[Pathname.new(File.expand_path('../', __FILE__)).join('support/**/*.rb')].each {|f| require f}
 
+Zxcvbn.build_ranked_dictionaries
+
 RSpec.configure do |config|
   config.include JsZxcvbnHelpers
 end
