60f28109e28190883675531743a7bcd736baea71
Matthieu Aussaguel <matthieu.aussaguel@gmail.com>
2012-09-25 14:13:03 +1000

    Improve spec naming

--- spec/scoring/entropy_spec.rb
+++ spec/scoring/entropy_spec.rb
@@ -10,14 +10,14 @@ describe Zxcvbn::Scoring::Entropy do
   }
 
   describe '#digits_entropy' do
-    it 'behaves the same as the js version' do
+    it 'returns a base 2 log of 10 to the power of the token length' do
       match = Zxcvbn::Match.new(:token => '12345678')
       entropy.digits_entropy(match).should eq lg(10 ** match.token.length)
     end
   end
 
   describe '#year_entropy' do
-    it 'behaves the same as the js version' do
+    it 'returns a base 2 log of the number of possible years' do
       entropy.year_entropy(nil).should eq lg(Zxcvbn::Scoring::Entropy::NUM_YEARS)
     end
   end
