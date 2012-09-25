9dbcaf230fd6da8b0a0948b3eb76c766bb988c30
Matthieu Aussaguel <matthieu.aussaguel@gmail.com>
2012-09-25 11:00:03 +1000

    Add more tests on the bruteforce cardinality calculation

--- lib/zxcvbn/score/math.rb
+++ lib/zxcvbn/score/math.rb
@@ -24,6 +24,31 @@ module Zxcvbn
         cardinality += 33 if is_type_of['symbols']
         cardinality
       end
+
+      # def display_time(seconds)
+      #   minute  = 60
+      #   hour    = minute * 60
+      #   day     = hour * 24
+      #   month   = day * 31
+      #   year    = month * 12
+      #   century = year * 100
+
+      #   if seconds < minute
+      #     'instant'
+      #   elsif seconds < hour
+      #     "#{1 + (seconds / minute).ceil} minutes"
+      #   else if seconds < day
+      #     "#{1 + (seconds / hour).ceil} hours"
+      #   else if seconds < month
+      #     "#{1 + (seconds / day).ceil} days"
+      #   else if seconds < year
+      #     "#{1 + (seconds / month).ceil} months"
+      #   else if seconds < century
+      #     "#{1 + (seconds / year).ceil} years"
+      #   else
+      #     'centuries'
+      #   end
+      # end
     end
   end    
 end
--- spec/score/math_spec.rb
+++ spec/score/math_spec.rb
@@ -4,10 +4,6 @@ describe Zxcvbn::Score do
   subject { described_class.new }
 
   describe '#bruteforce_cardinality' do
-    it 'returns the bruteforce cardinately for password' do
-      subject.bruteforce_cardinality('password').should eql 26
-    end
-
     context 'when empty password' do
       it 'should return 0 if empty password' do
         subject.bruteforce_cardinality('').should eql 0
@@ -44,6 +40,38 @@ describe Zxcvbn::Score do
           %w|/ [ ` {|.each do |symbol|
             subject.bruteforce_cardinality(symbol).should eql 33
           end
+        end
+      end
+    end
+
+    context 'when password is more than one character long' do
+      context 'and only digits' do
+        it 'should return 10' do
+          subject.bruteforce_cardinality('123456789').should eql 10
+        end
+      end
+
+      context 'and only lowercase characters' do
+        it 'should return 26' do
+          subject.bruteforce_cardinality('password').should eql 26
+        end
+      end
+
+      context 'and only uppercase characters' do
+        it 'should return 26' do
+          subject.bruteforce_cardinality('PASSWORD').should eql 26
+        end
+      end
+
+      context 'and only symbols' do
+        it 'should return 33' do
+          subject.bruteforce_cardinality('/ [ ` {').should eql 33
+        end
+      end
+
+      context 'and a mixed of character types' do
+        it 'should add up every character type cardinality' do
+          subject.bruteforce_cardinality('p1SsWorD!').should eql 95
         end
       end
     end
