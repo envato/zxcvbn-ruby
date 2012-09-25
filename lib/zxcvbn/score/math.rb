8f0c9f8194b2902caab5b8b9988445b83c6b7d30
Matthieu Aussaguel <matthieu.aussaguel@gmail.com>
2012-09-25 11:58:32 +1000

    Add specs on Math#display_time

--- lib/zxcvbn/score/math.rb
+++ lib/zxcvbn/score/math.rb
@@ -8,9 +8,9 @@ module Zxcvbn
           case ordinal
           when (48..57)
             is_type_of['digits'] = true
-          when(65..90)
+          when (65..90)
             is_type_of['upper'] = true
-          when(97..122)
+          when (97..122)
             is_type_of['lower'] = true
           else
             is_type_of['symbols'] = true
@@ -25,30 +25,31 @@ module Zxcvbn
         cardinality
       end
 
-      # def display_time(seconds)
-      #   minute  = 60
-      #   hour    = minute * 60
-      #   day     = hour * 24
-      #   month   = day * 31
-      #   year    = month * 12
-      #   century = year * 100
+      def display_time(seconds)
+        minute  = 60
+        hour    = minute * 60
+        day     = hour * 24
+        month   = day * 31
+        year    = month * 12
+        century = year * 100
 
-      #   if seconds < minute
-      #     'instant'
-      #   elsif seconds < hour
-      #     "#{1 + (seconds / minute).ceil} minutes"
-      #   else if seconds < day
-      #     "#{1 + (seconds / hour).ceil} hours"
-      #   else if seconds < month
-      #     "#{1 + (seconds / day).ceil} days"
-      #   else if seconds < year
-      #     "#{1 + (seconds / month).ceil} months"
-      #   else if seconds < century
-      #     "#{1 + (seconds / year).ceil} years"
-      #   else
-      #     'centuries'
-      #   end
-      # end
+        case
+        when seconds < minute
+          'instant'
+        when seconds < hour
+          "#{1 + (seconds / minute).ceil} minutes"
+        when seconds < day
+          "#{1 + (seconds / hour).ceil} hours"
+        when seconds < month
+          "#{1 + (seconds / day).ceil} days"
+        when seconds < year
+          "#{1 + (seconds / month).ceil} months"
+        when seconds < century
+          "#{1 + (seconds / year).ceil} years"
+        else
+          'centuries'
+        end        
+      end
     end
   end    
 end
