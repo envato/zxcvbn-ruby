811f2a6d30d5dd050102cfb22ff488c1a2a1ba3a
Mark IJbema <markijbema@gmail.com>
2014-05-23 17:35:36 +0200

    Removed modifying from re_match_all
    Used position parameter of match. This ought to be faster, and removes the need to duplicate strings before passing them in

--- lib/zxcvbn/matchers/regex_helpers.rb
+++ lib/zxcvbn/matchers/regex_helpers.rb
@@ -2,18 +2,18 @@ module Zxcvbn
   module Matchers
     module RegexHelpers
       def re_match_all(regex, password)
-        loop do
-          re_match = regex.match(password)
-          break unless re_match
+        pos = 0
+        while re_match = regex.match(password, pos)
           i, j = re_match.offset(0)
+          pos = j
           j -= 1
+
           match = Match.new(
             :i => i,
             :j => j,
             :token => password[i..j]
           )
           yield match, re_match
-          password = password.sub(re_match[0], ' ' * re_match[0].length)
         end
       end
     end
