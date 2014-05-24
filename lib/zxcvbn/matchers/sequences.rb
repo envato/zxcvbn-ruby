845a4ae34105877322690e0a23663b99e75c8f1b
Mark IJbema <markijbema@gmail.com>
2014-05-24 14:51:04 +0200

    Simplified some more

--- lib/zxcvbn/matchers/sequences.rb
+++ lib/zxcvbn/matchers/sequences.rb
@@ -12,43 +12,30 @@ module Zxcvbn
         i = 0
         while i < password.length - 1
           j = i + 1
-          found = false
           SEQUENCES.each do |seq_name, seq|
-            next if found
             next unless seq.index(password[i])
+            next unless seq.index(password[j])
             index_i = seq.index(password[i])
+            seq_direction = seq.index(password[j]) - seq.index(password[i])
+            next unless [-1, 1].include? seq_direction
 
-            [-1, 1].each do |seq_direction|
-              next if found
-              next unless seq.index(password[j]) == seq.index(password[i]) + seq_direction
-              loop do
-                cur_n = password[j] ? seq.index(password[j]) : nil
-                if cur_n == index_i + seq_direction * (j - i)
-                  j += 1
-                else
-                  if j - i > 2 # don't consider length 1 or 2 chains.
-                    result << Match.new(
-                      :pattern => 'sequence',
-                      :i => i,
-                      :j => j-1,
-                      :token => password[i...j],
-                      :sequence_name => seq_name,
-                      :sequence_space => seq.length,
-                      :ascending => seq_direction == 1
-                    )
-                  end
-                  found = true
-                  break
-                end
-              end
+            while j < password.length &&
+                  seq.index(password[j]) == index_i + seq_direction * (j - i)
+              j+= 1
+            end
+            if j - i > 2 # don't consider length 1 or 2 chains.
+              result << Match.new(
+                :pattern => 'sequence',
+                :i => i,
+                :j => j-1,
+                :token => password[i...j],
+                :sequence_name => seq_name,
+                :sequence_space => seq.length,
+                :ascending => seq_direction == 1
+              )
             end
           end
-          if j > i + 1
-            i = j - 1
-          else
-            i = i + 1
-          end
-          found = false
+          i = [i + 1, j - 1].max
         end
         result
       end
