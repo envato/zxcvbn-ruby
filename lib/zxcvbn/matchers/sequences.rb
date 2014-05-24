b2950aa07e044d2713590cea492028377b0e1808
Mark IJbema <markijbema@gmail.com>
2014-05-24 18:08:26 +0200

    Split up logic into methods

--- lib/zxcvbn/matchers/sequences.rb
+++ lib/zxcvbn/matchers/sequences.rb
@@ -7,35 +7,56 @@ module Zxcvbn
         'digits' => '01234567890'
       }
 
+      def seq_match_length(password, from, direction, seq)
+        index_from = seq.index(password[from])
+        j = 1
+        while from + j < password.length &&
+              password[from + j] == seq[index_from + direction * j]
+          j+= 1
+        end
+        j
+      end
+
+      # find the first matching sequence, and return with
+      # direction, if characters are one apart in the sequence
+      def applicable_sequence(password, i)
+        SEQUENCES.each do |name, sequence|
+          index1 = sequence.index(password[i])
+          index2 = sequence.index(password[i+1])
+          if index1 and index2
+            seq_direction = index2 - index1
+            if [-1, 1].include?(seq_direction)
+              return [name, sequence, seq_direction]
+            else
+              return nil
+            end
+          end
+        end
+      end
+
       def matches(password)
         result = []
         i = 0
         while i < password.length - 1
-          j = i + 1
-          SEQUENCES.each do |seq_name, seq|
-            next unless seq.index(password[i])
-            next unless seq.index(password[j])
-            index_i = seq.index(password[i])
-            seq_direction = seq.index(password[j]) - seq.index(password[i])
-            next unless [-1, 1].include? seq_direction
+          seq_name, seq, seq_direction = applicable_sequence(password, i)
 
-            while j < password.length &&
-                  password[j] == seq[index_i + seq_direction * (j - i)]
-              j+= 1
-            end
-            if j - i > 2 # don't consider length 1 or 2 chains.
+          if seq
+            length = seq_match_length(password, i, seq_direction, seq)
+            if length > 2
               result << Match.new(
                 :pattern => 'sequence',
                 :i => i,
-                :j => j-1,
-                :token => password[i...j],
+                :j => i + length - 1,
+                :token => password[i, length],
                 :sequence_name => seq_name,
                 :sequence_space => seq.length,
                 :ascending => seq_direction == 1
               )
             end
+            i += length - 1
+          else
+            i += 1
           end
-          i = [i + 1, j - 1].max
         end
         result
       end
