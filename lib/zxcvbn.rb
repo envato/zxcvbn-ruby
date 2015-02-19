c6473d0f0fa88cb90b4687243dd5a80ca3d3aab1
Tim Lucas <t@toolmantim.com>
2015-02-19 20:17:36 +1100

    Switch from YAML to txt files
    * Loads in half the time
    * Requires half the memory to load

--- lib/zxcvbn.rb
+++ lib/zxcvbn.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'pathname'
+
 require 'zxcvbn/version'
 require 'zxcvbn/match'
 require 'zxcvbn/matchers/regex_helpers'
@@ -23,7 +26,14 @@ module Zxcvbn
 
   DATA_PATH = Pathname(File.expand_path('../../data', __FILE__))
   ADJACENCY_GRAPHS = JSON.load(DATA_PATH.join('adjacency_graphs.json').read)
-  FREQUENCY_LISTS = YAML.load(DATA_PATH.join('frequency_lists.yaml').read)
+  FREQUENCY_LISTS_PATH = DATA_PATH.join("frequency_lists")
+  FREQUENCY_LISTS = {
+    "english":      FREQUENCY_LISTS_PATH.join("english.txt").read.split,
+    "female_names": FREQUENCY_LISTS_PATH.join("female_names.txt").read.split,
+    "male_names":   FREQUENCY_LISTS_PATH.join("male_names.txt").read.split,
+    "passwords":    FREQUENCY_LISTS_PATH.join("passwords.txt").read.split,
+    "surnames":     FREQUENCY_LISTS_PATH.join("surnames.txt").read.split
+  }
   RANKED_DICTIONARIES = DictionaryRanker.rank_dictionaries(FREQUENCY_LISTS)
 
   def test(password, user_inputs = [])
