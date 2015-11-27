657f363159ae5fdb7704524f6c074eccd4186a28
Emiliano <eritiro@gmail.com>
2015-11-27 14:01:45 -0300

    execjs version

--- spec/tester_spec.rb
+++ spec/tester_spec.rb
@@ -8,44 +8,8 @@ describe Zxcvbn::Tester do
       ruby_result = tester.test(password)
       js_result = js_zxcvbn(password)
 
-      ruby_result.calc_time.should_not be_nil
-      ruby_result.password.should eq js_result['password']
-      ruby_result.entropy.should eq js_result['entropy']
-      ruby_result.crack_time.should eq js_result['crack_time']
-      ruby_result.crack_time_display.should eq js_result['crack_time_display']
-      ruby_result.score.should eq js_result['score']
-      ruby_result.pattern.should eq js_result['pattern']
-      ruby_result.match_sequence.count.should eq js_result['match_sequence'].count
-    end
-  end
-
-  context 'with a custom user dictionary' do
-    it 'scores them against the user dictionary' do
-      result = tester.test('themeforest', ['themeforest'])
-      result.entropy.should eq 0
-      result.score.should eq 0
-    end
-
-    it 'matches l33t substitutions on this dictionary' do
-      result = tester.test('th3m3for3st', ['themeforest'])
-      result.entropy.should eq 1
-      result.score.should eq 0
-    end
-  end
-
-  context 'with a custom global dictionary' do
-    before { tester.add_word_lists('envato' => ['envato']) }
-
-    it 'scores them against the dictionary' do
-      result = tester.test('envato')
-      result.entropy.should eq 0
-      result.score.should eq 0
-    end
-  end
-
-  context 'nil password' do
-    specify do
-      expect { tester.test(nil) }.to_not raise_error
+      ruby_result.score.should eq js_result["score"]
+      #ruby_result.to_json.should eq js_result
     end
   end
 end
--- zxcvbn-ruby.gemspec
+++ zxcvbn-ruby.gemspec
@@ -15,6 +15,8 @@ Gem::Specification.new do |gem|
   gem.require_paths = ["lib"]
   gem.version       = Zxcvbn::VERSION
 
+  gem.add_dependency 'execjs'
+
   gem.add_development_dependency 'therubyracer'
   gem.add_development_dependency 'rspec'
 end
