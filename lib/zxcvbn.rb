2ac5cae4ea0434045762d4a476f23f1da5782317
Alexis <alexis@cvofhope.com>
2018-08-09 13:14:52 +1000

    add feedback function to zxcvbn

--- lib/zxcvbn.rb
+++ lib/zxcvbn.rb
@@ -1,6 +1,7 @@
 require 'pathname'
 require 'zxcvbn/version'
 require 'zxcvbn/tester'
+require 'zxcvbn/feedback'
 
 module Zxcvbn
   extend self
@@ -16,5 +17,9 @@ module Zxcvbn
     tester = Tester.new
     tester.add_word_lists(word_lists)
     tester.test(password, user_inputs)
+  end
+
+  def feedback(score)
+    Feedback.new(score.score, score.match_sequence).suggestions
   end
 end
