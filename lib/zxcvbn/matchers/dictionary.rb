19fadc5d65966ad9d2b07d6f4762494a054978eb
Orien Madgwick <497874+orien@users.noreply.github.com>
2025-12-07 06:26:18 +0700

    RuboCop: address `Style/PreferredHashMethods` issues

--- lib/zxcvbn/matchers/dictionary.rb
+++ lib/zxcvbn/matchers/dictionary.rb
@@ -20,7 +20,7 @@ module Zxcvbn
         (0..password_length).each do |i|
           (i...password_length).each do |j|
             word = lowercased_password[i..j]
-            if @ranked_dictionary.has_key?(word)
+            if @ranked_dictionary.key?(word)
               results << Match.new(
                 matched_word: word,
                 token: password[i..j],
